d8c684fb88c23937bb3cf5ada18b3f72e13529ce
Daniel Fernandes de Moraes <ti.danielmoraes@gmail.com>
2025-11-07 18:38:43 +0000

    Colocados os 3 console.log

---
+++ app.js
@@ -0,0 +1,11 @@
+const taxaCambio = 0.92;
+const moedaOrigem = 'Euro';
+const moedaDestino = 'Dólar';
+
+let valorEmEuro = '500';
+
+let valorEmDolar = valorEmEuro * taxaCambio;
+
+console.log('Tipo de dados em valorEmEuro antes da conversão:', typeof valorEmEuro);
+console.log('Tipo de dados em valorEmDolar depois da conversão:', typeof valorEmDolar);
+console.log('500 Euros equivalem a ' + valorEmDolar + ' Dólares');
